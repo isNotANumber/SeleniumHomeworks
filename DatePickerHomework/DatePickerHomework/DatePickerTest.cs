d1cdc2197912ca7028536ed46f3fc152a1d409b7
Титов Кирилл Артемович <titov.ka@skbkontur.ru>
2022-07-11 18:14:26 +0500

    some test fixes

--- DatePickerHomework/DatePickerHomework/DatePickerTest.cs
+++ DatePickerHomework/DatePickerHomework/DatePickerTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -8,7 +9,19 @@ namespace DatePickerHomework
     public class DatePickerTest
     {
         private IWebDriver driver;
-        
+
+        private string FormatDate(DateTime date)
+        {
+            return date.ToString("MM'/'dd'/'yyyy");
+        }
+
+        private string FormatDate(string date)
+        {
+            var resultDate = DateTime.Parse(date, CultureInfo.InvariantCulture);
+
+            return resultDate.ToString("MM'/'dd'/'yyyy");
+        }
+
         [SetUp]
         public void SetUp()
         {
@@ -27,15 +40,19 @@ namespace DatePickerHomework
             var frameElement = driver.FindElement(By.TagName("iframe"));
             driver.SwitchTo().Frame(frameElement);
 
-            var date = DateTime.Today.AddDays(8).ToString("dd-MM-yyyy").Replace("-", "/");
+            var date = FormatDate(DateTime.Today.AddDays(8));
 
             (driver as IJavaScriptExecutor).ExecuteScript($"$('#datepicker').datepicker('setDate', '{date}')");
             
-            var calendar = driver.FindElement(By.Id("datepicker"));
+            // Варианты извлечения значения:
+            // var calendar = driver.FindElement(By.Id("datepicker"));
+            // var resultDate = FormatDate(calendar.GetAttribute("value"));
+            // var resultDate = FormatDate((driver as IJavaScriptExecutor).ExecuteScript("return $('#datepicker').datepicker('getDate')").ToString());
             
-            var resultDate = calendar.GetAttribute("value");
+            // Финальный вариант:
+            var resultDate = FormatDate((driver as IJavaScriptExecutor).ExecuteScript("return $('#datepicker').val();").ToString());
             
-            Assert.AreEqual(date, resultDate);
+            Assert.AreEqual(date, resultDate, "Дата выставлена некорректно!");
         }
         
         [TearDown]
